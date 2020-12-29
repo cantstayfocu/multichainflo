eaaea9b4b41bc09b279dc4ab52e4a784d2ed60f1
Channing Cox <51252047+cantstayfocu@users.noreply.github.com>
2020-12-28 22:28:35 -0500

    Update address.go
    copypasta from the readme

--- chain/flo/address.go
+++ chain/flo/address.go
@@ -1,3 +1,9 @@
 package flo
 
+import "github.com/renproject/multichain/chain/flo"
 
+type (
+	AddressEncoder       = bitcoin.AddressEncoder
+	AddressDecoder       = bitcoin.AddressDecoder
+	AddressEncodeDecoder = bitcoin.AddressEncodeDecoder
+)
